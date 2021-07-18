8f2ab86e80c9f781d78dc711ddaedd0240f218d8
Tsubasa Ozawa <t.ozawa.sake@gmail.com>
2021-07-19 00:14:49 +0900

    s3へのファイルアップロード初歩

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func main() {
+	sess, err := session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Region: aws.String("ap-northeast-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"your accsess id", // AWS ACCESS KEY
+				"your sercret id", // AWS SECRET KEY
+				"token",           // Token ※Tokenを使用していない場合は空文字を設定
+			),
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// ファイルを開く
+	targetFilePath := "./sample.txt"
+	file, err := os.Open(targetFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	bucketName := "teamup-t"
+	objectKey := "user-image/sample.txt"
+
+	// Uploaderを作成し、ローカルファイルをアップロード
+	uploader := s3manager.NewUploader(sess)
+	ret, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectKey),
+		Body:   file,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(ret.Location)
+	log.Println("done")
+}
